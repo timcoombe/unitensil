dadd128e75212f878e34a3b8acb610530189fbba
Tim Coombe <tim.coombe@bskyb.com>
2015-04-25 15:32:39 +0100

    Added swipe to ViewController

--- Unitensil/ViewController.swift
+++ Unitensil/ViewController.swift
@@ -15,10 +15,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let url = NSURL (string: "http://unitensilapp.com/android/app3/");
+        let url = NSURL (string: "http://unitensilapp.com/ios/app/");
         let requestObj = NSURLRequest(URL: url!);
         myWebView.scalesPageToFit = true
         myWebView.loadRequest(requestObj);
+        
+        
+        
+        var swipeRight = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        var swipeLeft = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(swipeLeft)
+
         
 
     }
@@ -26,6 +37,25 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.Right:
+                myWebView.goBack()
+                println("Swiped right")
+            case UISwipeGestureRecognizerDirection.Left:
+                 myWebView.goForward()
+                
+                
+                println("Swiped left")
+            default:
+                break
+            }
+        }
     }
 
 }
